b6cbf25568e2e5947213b846fbfd730bc0b55735
princestoretransform-spec <princestoretransform@gmail.com>
2026-04-10 10:30:47 +0530

    .

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,6 +1,6 @@
 
-import { AboutHero, Exchanges, Footer, Header, PremiumNews } from "@/components";
-import ServiceHighlight from "@/components/About/ServicesGrid";
+import { AboutHero, ServiceHighlight,Exchanges, Footer, Header, PremiumNews } from "@/components";
+// import ServiceHighlight from "@/components/About/ServicesGrid";
 
 const Service = () => {
   return (
